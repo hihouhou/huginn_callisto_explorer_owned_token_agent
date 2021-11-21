1c52be374f30b998120672595a81853275eb61a0
Nicolas Germain <ngermain@hihouhou.com>
2021-11-21 11:40:00 +0100

    decimal value added for final resul

--- lib/huginn_callisto_explorer_owned_token_agent/callisto_explorer_owned_token_agent.rb
+++ lib/huginn_callisto_explorer_owned_token_agent/callisto_explorer_owned_token_agent.rb
@@ -13,6 +13,8 @@ module Agents
 
       `debug` is used for verbose mode.
 
+      `decimal` for adding value with token decimal.
+
       The `changes only` option causes the Agent to report an event only when the status changes. If set to false, an event will be created for every check.  If set to true, an event will only be created when the status changes (like if your site goes from 200 to 500).
 
       `expected_receive_period_in_days` is used to determine if the Agent is working. Set it to the maximum number of days
@@ -29,7 +31,8 @@ module Agents
             "decimals": "18",
             "name": "Callisto Enterprise",
             "symbol": "CLOE",
-            "type": "ERC-20"
+            "type": "ERC-20",
+            "value": "13641.540356782800000000"
           }
     MD
 
@@ -37,6 +40,7 @@ module Agents
       {
         'debug' => 'false',
         'changes_only' => 'true',
+        'decimal' => 'true',
         'expected_receive_period_in_days' => '2',
         'wallet' => ''
       }
@@ -44,9 +48,14 @@ module Agents
 
     form_configurable :changes_only, type: :boolean
     form_configurable :debug, type: :boolean
+    form_configurable :decimal, type: :boolean
     form_configurable :wallet, type: :string
     form_configurable :expected_receive_period_in_days, type: :string
     def validate_options
+      if options.has_key?('decimal') && boolify(options['decimal']).nil?
+        errors.add(:base, "if provided, decimal must be true or false")
+      end
+
       if options.has_key?('debug') && boolify(options['debug']).nil?
         errors.add(:base, "if provided, debug must be true or false")
       end
@@ -99,6 +108,11 @@ module Agents
                 if interpolated['debug'] == 'true'
                   log event
                 end
+                if interpolated['decimal'] == 'true'
+                  power = 10 ** token['decimal'].to_i
+                  value = event['balance'].to_f / power.to_i
+                  event.merge!({ :value => value })
+                end
                 create_event payload: event
               end
             else
@@ -122,6 +136,11 @@ module Agents
                   if interpolated['debug'] == 'true'
                     log "found is #{found}! so token created"
                     log token
+                  end
+                  if interpolated['decimal'] == 'true'
+                    power = 10 ** token['decimal'].to_i
+                    value = token['balance'].to_f / power.to_i
+                    token.merge!({ :value => value })
                   end
                   create_event payload: token
                 end
